2e23cb0cef4581f07c16734b256206bacc9dbdcf
Fabio Colella <fcole90@gmail.com>
2016-11-24 22:52:00 +0100

    Added functions to retrieve scripts

--- keywords/imdb_interface.py
+++ keywords/imdb_interface.py
@@ -6,7 +6,9 @@ from tools.thread_manager import Operation
 from tools import url_retriever as ur
 from tools import generic_helpers as helpers
 
+
 __delay__ = 1
+
 
 class BadResponseError(Exception):
     def __init__(self, value, query=None):
@@ -15,7 +17,7 @@ class BadResponseError(Exception):
 
     def __str__(self):
         message = ("IMDB Error" +
-            " - " + self.value)
+                   " - " + self.value)
         if self.query:
             message += " - Query: '" + str(self.query) + "'"
         return message
@@ -23,12 +25,13 @@ class BadResponseError(Exception):
     def get_response(self):
         return self.value
 
-
     def get_query(self):
         return self.query
 
+
 def get_imdb_id(title, year=None):
     return get_imdb_data(title, year)['imdbID']
+
 
 def get_imdb_data(title, year=None):
     query_title = '+'.join(helpers.clean_string(title).split())
@@ -55,40 +58,48 @@ def get_imdb_data(title, year=None):
 
 def get_title_and_year(movielens_title):
     if '(' in movielens_title:
-        title_list = movielens_title.split('(') # "Toy Story (1995)" -> ["Toy Story", "1995)"]
+        title_list = movielens_title.split('(')  # "Toy Story (1995)" -> ["Toy Story", "1995)"]
         title = title_list[0]
-        year = helpers.clean_number(title_list[-1]) # Get the clean year
-        return (title, year)
+        year = helpers.clean_number(title_list[-1])  # Get the clean year
+        return title, year
     else:
-        return (movielens_title, None)
+        return movielens_title, None
+
 
 def get_imdb_keywords(imdb_id):
     soup = ur.get_soup("http://www.imdb.com/title/" + imdb_id + "/keywords")
-    keywords = [el.get('data-item-keyword') for el in soup.find_all('td',class_="soda sodavote")]
+    keywords = [el.get('data-item-keyword') for el in soup.find_all('td', class_="soda sodavote")]
     return keywords
+
 
 def get_imdb_from_movielens(movielens_title):
     title_and_year = get_title_and_year(movielens_title)
     return get_imdb_id(title_and_year[0], title_and_year[1])
 
+
 def get_imdb_data_from_movielens(movielens_title):
     title_and_year = get_title_and_year(movielens_title)
     return get_imdb_data(title_and_year[0], title_and_year[1])
 
+
 def get_keywords_from_movielens(movielens_title):
     return get_imdb_keywords(get_imdb_from_movielens(movielens_title))
 
+
 def save_imdb_id(movielens_id, imdb_id, imdb_file, imdb_title):
-    line = ('::'.join([movielens_id, imdb_id, imdb_title]) ) + '\n'
+    line = ('::'.join([movielens_id, imdb_id, imdb_title])) + '\n'
     imdb_file.write(line)
+
 
 def save_keywords(movielens_id, keywords, keywords_file):
     line = '::'.join([movielens_id, '|'.join(keywords)]) + '\n'
     keywords_file.write(line)
 
+
 def save_failures(movielens_id, error_dict, error_file):
     line = movielens_id + '::' + error_dict + '\n'
     error_file.write(line)
+
 
 def wrapper_imdb_retrieve_data(args):
     movie_data = args['movie_data']
@@ -109,6 +120,7 @@ def wrapper_imdb_retrieve_data(args):
             save_failures(args['movie_data']['id'], str(e), args['failures_keywords_file'])
 
     print(identifier + "Retrivial complete..")
+
 
 def get_all_keywords_and_imdb_id_from_movielens(movie_data, imdb_file, keywords_file, failures_keywords_file):
     manager = ThreadManager()
@@ -136,3 +148,28 @@ def get_all_keywords_and_imdb_id_from_movielens(movie_data, imdb_file, keywords_
             break
         time.sleep(__delay__)
 
+
+def get_script_url(movielens_title):
+    url_title = get_title_and_year(movielens_title)[0]  # Get only title
+    url_title = '-'.join(url_title.split())  # Reformat the title: Toy Story -> Toy-Story
+    return url_title
+
+
+def get_imsdb_script_full_url(movielens_title):
+    url_title = get_script_url(movielens_title)
+    full_url = "http://www.imsdb.com/scripts/" + url_title + ".html"
+    return full_url
+
+
+def get_imsdb_script_text(movielens_title):
+    url = get_imsdb_script_full_url(movielens_title)
+    soup = ur.get_soup(url)
+
+    return soup.find_all('td', class_='scrtext')[0].get_text()
+
+
+def is_full_script(text):
+    if len(text) < 100:
+        return False
+    else:
+        return True
